89c194c5cd4e2b753e4dc1a1d74d767f6bf57cf2
ArtelordTheGreat <arrrick25@gmail.com>
2025-11-06 02:24:46 +0300

    Update results.js

--- results.js
+++ results.js
@@ -6,3 +6,14 @@ window.onload = () => {
   }
   document.getElementById("result-text").innerText = output;
 };
+
+function getArchetype(scores) {
+  if (scores.order > scores.chaos && scores.divine > scores.heretic)
+    return "The Celestial Bureaucrat";
+  if (scores.chaos > scores.order && scores.heretic > scores.divine)
+    return "The Laughing Heresiarch";
+  return "The Undefined Wanderer";
+}
+
+document.getElementById("result-text").innerText = 
+  `You are: ${getArchetype(scores)}\n\n` + output;
