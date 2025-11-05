6027aa8168738aa8de10e189f4dc06921c7396fe
ArtelordTheGreat <arrrick25@gmail.com>
2025-11-06 02:17:41 +0300

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,18 @@
+const questions = [
+  {
+    question: "Only through structure can the universe sing in harmony.",
+    effect: { order: 10, chaos: -10 }
+  },
+  {
+    question: "Rust and vines are both forms of growth.",
+    effect: { mech: -5, nature: 5 }
+  },
+  {
+    question: "To defy the divine is to embrace freedom.",
+    effect: { divine: -10, heretic: 10 }
+  },
+  {
+    question: "Instinct is wiser than calculation.",
+    effect: { rational: -8, esoteric: 8 }
+  }
+];
