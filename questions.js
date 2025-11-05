17fe13d87cecfb5719d1a52c7b1e0d6e8745b60a
ArtelordTheGreat <arrrick25@gmail.com>
2025-11-06 02:49:20 +0300

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,18 +1,18 @@
 const questions = [
   {
     question: "Only through structure can the universe sing in harmony.",
-    effect: { order: 10, chaos: -10 }
+    effect: { order: 10 } // +10 = Order, -10 = Chaos
   },
   {
     question: "Rust and vines are both forms of growth.",
-    effect: { mech: -5, nature: 5 }
+    effect: { mech: -10 } // -10 = Nature, +10 = Mech
   },
   {
     question: "To defy the divine is to embrace freedom.",
-    effect: { divine: -10, heretic: 10 }
+    effect: { divine: -10 } // -10 = Heretic, +10 = Divine
   },
   {
     question: "Instinct is wiser than calculation.",
-    effect: { rational: -8, esoteric: 8 }
+    effect: { rational: -10 } // -10 = Esoteric, +10 = Rational
   }
 ];
