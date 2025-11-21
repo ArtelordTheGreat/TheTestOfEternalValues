8301ea5eec9e5f69d76d2442cd09de87026d1416
ArtelordTheGreat <arrrick25@gmail.com>
2025-11-21 18:29:02 +0300

    Create archetypes.js

---
+++ archetypes.js
@@ -0,0 +1,47 @@
+const archetypes = [
+  {
+    name: "Purifying Paladin",
+    stats: {
+      order: 80,
+      mechanism: 60,
+      divine: 60,
+      rational: 20
+    }
+  },
+  {
+    name: "Techno-Druid",
+    stats: {
+      order: -30,
+      mechanism: 70,
+      divine: 10,
+      rational: -10
+    }
+  },
+  {
+    name: "Heretical Inventor",
+    stats: {
+      order: -40,
+      mechanism: 80,
+      divine: -60,
+      rational: 70
+    }
+  },
+  {
+    name: "Sacred Hermit",
+    stats: {
+      order: 10,
+      mechanism: -40,
+      divine: 80,
+      rational: -20
+    }
+  },
+  {
+    name: "Balanced Observer",
+    stats: {
+      order: 0,
+      mechanism: 0,
+      divine: 0,
+      rational: 0
+    }
+  }
+];
