f26ef9f68261432368083d4ea915e0eab87356dc
ArtelordTheGreat <arrrick25@gmail.com>
2025-11-24 23:36:33 +0300

    shuffle questions

--- quiz.js
+++ quiz.js
@@ -17,12 +17,20 @@ function nextQuestion(multiplier) {
   showQuestion();
 }
 
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
 function finishQuiz() {
   localStorage.setItem("epicScores", JSON.stringify(scores));
   window.location.href = "results.html";
 }
 
 window.onload = function() {
+  shuffle(questions);
   initScores();
   showQuestion();
 };
