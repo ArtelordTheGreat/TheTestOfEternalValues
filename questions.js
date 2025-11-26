223420513391579d25b1a5cc4528d2b5fa6178e7
ArtelordTheGreat <arrrick25@gmail.com>
2025-11-27 00:54:34 +0300

    There are now 40 questions

--- questions.js
+++ questions.js
@@ -1,30 +1,162 @@
 let questions = [
   {
     question: "Only through structure can the universe sing in harmony.",
-    effect: { order: 10 } // +10 = Order, -10 = Chaos
-  },
-  {
-    question: "Rust and vines are both forms of growth.",
-    effect: { mech: -10 } // -10 = Nature, +10 = Mech
-  },
-  {
-    question: "To defy the divine is to embrace freedom.",
-    effect: { divine: -10 } // -10 = Heretic, +10 = Divine
-  },
-  {
-    question: "Instinct is wiser than calculation.",
-    effect: { rational: -10 } // -10 = Esoteric, +10 = Rational
+    effect: { order: 5 } //order
   },
   {
     question: "Only through iron discipline we will be able to overcome what's ahead of us.",
-    effect: { order: 10 }
+    effect: { order: 5 }
+  },
+  {
+    question: "In order there is strength, it is what gives both power and peace.",
+    effect: { order: 5 }
+  },
+  {
+    question: "Strict rules must be set in place and be followed.",
+    effect: { order: 5 }
+  },
+  {
+    question: "We must throw away personal impulses for the sake of being organised.",
+    effect: { order: 5 }
+  },
+  {
+    question: "It is better to act not according to plans, but to momentary impulses that push you.",
+    effect: { order: -5 } //chaos
+  },
+  {
+    question: "The only way to be free and happy is to don't follow rules that others made.",
+    effect: { order: -5 }
+  },
+  {
+    question: "Live by your own pace and desire and then you'll find harmony.",
+    effect: { order: -5 }
+  },
+  {
+    question: "This life should be lived through by doing only what you feel like doing.",
+    effect: { order: -5 }
+  },
+  {
+    question: "Laws keep us restricted - it's better to abolish them and free inner avarity.",
+    effect: { order: -5 }
+  },
+  {
+    question: "Flesh is weak, mankind must use more steel instead.",
+    effect: { mech: 5 } //mechanism
+  },
+  {
+    question: "If we want to become immortal or to live longer, we have embrace transhumanism.",
+    effect: { mech: 5 }
+  },
+  {
+    question: "It is a good thing if the mankind keeps on replacing everything with machines.",
+    effect: { mech: 5 }
+  },
+  {
+    question: "Artificial constructs are trustworthy and better tools of mankind.",
+    effect: { mech: 5 }
+  },
+  {
+    question: "Humanity should forge its own future out of code and metal.",
+    effect: { mech: 5 }
+  },
+  {
+    question: "Rust and vines are both forms of growth.",
+    effect: { mech: -5 } //nature
+  },
+  {
+    question: "Humans are children of Mother Nature and we should stay loyal to here.",
+    effect: { mech: -5 }
+  },
+  {
+    question: "Plants and animals are our relatives - they also should have rights.",
+    effect: { mech: -5 }
+  },
+  {
+    question: "In harmony and synthesis with nature we will find harmony.",
+    effect: { mech: -5 }
+  },
+  {
+    question: "The closer a person to trees, grass and animals is, the better his or her life is.",
+    effect: { mech: -5 }
+  },
+  {
+    question: "To reach Heaven or inner peace one must follow divinity.",
+    effect: { divine: 5 } //divine
+  },
+  {
+    question: "The reason we perish is all sins that we did.",
+    effect: { divine: 5 }
+  },
+  {
+    question: "Any person becomes better when they confess and turn to the light.",
+    effect: { divine: 5 }
+  },
+  {
+    question: "One who serves the greater good becomes a good person.",
+    effect: { divine: 5 }
+  },
+  {
+    question: "All saints are people who did something incredible and marvelous.",
+    effect: { divine: 5 }
+  },
+  {
+    question: "To defy the divine is to embrace freedom.",
+    effect: { divine: -5 } //heretic
+  },
+  {
+    question: "Heretics are not wrongful - they're challenging oppressors.",
+    effect: { divine: -5 }
+  },
+  {
+    question: "Power comes to those who dare to challenge morals.",
+    effect: { divine: -5 }
+  },
+  {
+    question: "Satan is the first freethinker and the emancipator of worlds.",
+    effect: { divine: -5 }
+  },
+  {
+    question: "Sins are our strikes to be what we want to be and rightful acts.",
+    effect: { divine: -5 }
   },
   {
     question: "Science can explain anything in the world if given enough time.",
-    effect: { rational: 10 }
+    effect: { rational: 5 } //rational
   },
   {
-    question: "Flesh is weak, mankind must use more machinary instead.",
-    effect: { mech: 10 }
+    question: "A statement is true only if proved by logic and evidence.",
+    effect: { rational: 5 }
+  },
+  {
+    question: "To achieve anything we must stay rational when making decisions.",
+    effect: { rational: 5 }
+  },
+  {
+    question: "Emotions can drive us off but we must stay with rationality.",
+    effect: { rational: 5 }
+  },
+  {
+    question: "We should not trust what cannot be explained logically.",
+    effect: { rational: 5 }
+  },
+  {
+    question: "Instinct is wiser than calculation.",
+    effect: { rational: -5 } //irrational
+  },
+  {
+    question: "Emotions and faith can also be sources of knowledge.",
+    effect: { rational: -5 }
+  },
+  {
+    question: "Reason and empirical evidence alone are not enough to explain everything.",
+    effect: { rational: -5 }
+  },
+  {
+    question: "To gain deeper understanding of the world one should turn to unconscious processes.",
+    effect: { rational: -5 }
+  },
+  {
+    question: "Truth is not universal but subjective and individual.",
+    effect: { rational: -5 }
   }
 ];
